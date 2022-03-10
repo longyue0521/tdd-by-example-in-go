13c44ef1552ecaba47f849334988c336a9e6a21f
longyue0521 <longyueli0521@gmail.com>
2022-03-10 23:36:18 +0800

    ch4: [test] refactor test to assert.Equal() with Dollar instance, instead of amount value in test

--- money/money_test.go
+++ money/money_test.go
@@ -11,7 +11,7 @@ import (
 func TestMultiplication(t *testing.T) {
 	five := NewDollar(5)
 	product := five.Times(2)
-	assert.Equal(t, 10, product.Amount())
+	assert.Equal(t, NewDollar(10), product)
 	product = five.Times(3)
 	assert.Equal(t, 15, product.Amount())
 }
