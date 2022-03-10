1ce87ad7cfca6337836ffb63d3a749de41cf2ab4
longyue0521 <longyueli0521@gmail.com>
2022-03-10 22:13:15 +0800

    ch2: [test] redefine Times method, it should return another Dollar

--- money/money_test.go
+++ money/money_test.go
@@ -10,8 +10,8 @@ import (
 
 func TestMultiplication(t *testing.T) {
 	five := NewDollar(5)
-	five.Times(2)
-	assert.Equal(t, 10, five.Amount())
-	five.Times(3)
-	assert.Equal(t, 15, five.Amount())
+	product := five.Times(2)
+	assert.Equal(t, 10, product.Amount())
+	product = five.Times(3)
+	assert.Equal(t, 15, product.Amount())
 }
