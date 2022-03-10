0f66133613d3ecf943215b9930c82598ed7ca23a
longyue0521 <longyueli0521@gmail.com>
2022-03-10 20:49:57 +0800

    ch1: [test] test 5 dollars times(2) should be 10

--- money/money_test.go
+++ money/money_test.go
@@ -1,1 +1,13 @@
+package money_test
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMultiplication(t *testing.T) {
+	five := Dollar(5)
+	five.times(2)
+	assert.Equal(t, 10, five.amount)
+}
