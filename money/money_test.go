380c772f696834c975aa9192b0c6ab1da892898f
longyue0521 <longyueli0521@gmail.com>
2022-03-10 23:53:31 +0800

    ch5: [test] add Franc test case

--- money/money_test.go
+++ money/money_test.go
@@ -22,3 +22,9 @@ func TestEqualityByEquals(t *testing.T) {
 	assert.True(t, NewDollar(3).Equals(NewDollar(3)))
 	assert.False(t, NewDollar(6).Equals(NewDollar(7)))
 }
+
+func TestFrancMultiplication(t *testing.T) {
+	five := NewFranc(5)
+	assert.Equal(t, NewFranc(10), five.Times(2))
+	assert.Equal(t, NewFranc(15), five.Times(3))
+}
