4df36a6423e772bc31f1409f3936dda0bd11b136
longyue0521 <longyueli0521@gmail.com>
2022-03-15 21:02:20 +0800

    ch10: [refactor] extract newFranc function

--- money/franc.go
+++ money/franc.go
@@ -11,3 +11,7 @@ func NewFranc(amount int) Money {
 func (d *Franc) Times(multiplier int) Money {
 	return &Franc{NewMoney(d.amount*multiplier, d.currency)}
 }
+
+func newFranc(amount int, currency string) Money {
+	return &Franc{NewMoney(amount, currency)}
+}
