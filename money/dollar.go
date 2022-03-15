cd37b7e0888c1c32224ef4719d778c0cd8caf9c6
longyue0521 <longyueli0521@gmail.com>
2022-03-15 15:03:07 +0800

    ch6: let Dollar struct implements Amount() method of Money interface

--- money/dollar.go
+++ money/dollar.go
@@ -12,6 +12,10 @@ func (d *Dollar) Times(multiplier int) *Dollar {
 	return &Dollar{&money{amount: d.amount * multiplier}}
 }
 
+func (d *Dollar) Amount() int {
+	return d.amount
+}
+
 func (d *Dollar) Equals(dollar *Dollar) bool {
 	return d.amount == dollar.amount
 }
