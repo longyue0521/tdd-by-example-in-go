0bd420e732559de10301ef1a87818edb3190a10e
longyue0521 <longyueli0521@gmail.com>
2022-03-15 15:13:07 +0800

    ch6: [refactor] rename parameter "dollar" to "money" to match its type

--- money/dollar.go
+++ money/dollar.go
@@ -16,6 +16,6 @@ func (d *Dollar) Amount() int {
 	return d.amount
 }
 
-func (d *Dollar) Equals(dollar Money) bool {
-	return d.Amount() == dollar.Amount()
+func (d *Dollar) Equals(money Money) bool {
+	return d.Amount() == money.Amount()
 }
